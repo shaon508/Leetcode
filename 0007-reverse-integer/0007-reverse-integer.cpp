88961352605f1ef9e8c5ef3bec512757e265af5f
Shaon Roy <113965094+shaon508@users.noreply.github.com>
2024-01-02 12:54:11 +0600

    Time: 2 ms (38.68%), Space: 6.1 MB (99.01%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int reverse(int x) {
-        int sum = 0;
+         int sum = 0;
         while (x) {
             if (sum > INT_MAX / 10 || sum < INT_MIN / 10)
                 return 0;
