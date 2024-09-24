97e92eb460c0430fc7725de30a1e601b138999dc
Shaon Roy <113965094+shaon508@users.noreply.github.com>
2024-09-24 13:58:20 +0600

    Time: 0 ms (100.00%), Space: 8.8 MB (6.60%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     int strStr(string s, string t) {
-        int n=s.size(),m=t.size(),i=0,j,k,c=0;
-        while(i+m<=n)
-        {
-           j=i,c=0,k=0;
-            while((j<n && k<m) && s[j]==t[k])
-            {
-                j++,k++,c++;
-            }
-            if(c==m)return i;
-            i++;
-        }
-        return -1;
+           long long int mod = 1000000007;
+    long long int P = 29;
+    vector<long long int> v(1009);
+    v[0] = 1;
+    for (int i = 1; i < 1005; i++) {
+        v[i] = (v[i - 1] * P) % mod;
+    }
+
+    int n = s.size(), m = t.size(), sHash = 0, tHash = 0;
+    if (n < m) return -1;
+
+    for (int i = 0; i < m; i++) {
+        tHash = (tHash + (t[i] - 'a' + 1) * v[m - 1 - i]) % mod;
+        if(tHash < 0)tHash = (tHash + mod) % mod;
+        sHash = (sHash + (s[i] - 'a' + 1) * v[m - 1 - i]) % mod;
+        if(sHash < 0)sHash = (sHash + mod) % mod;
+    }
+
+    if (tHash == sHash && s.substr(0, m) == t) return 0;
+
+    for (int i = m; i < n; i++) {
+        sHash = (sHash - (s[i - m] - 'a' + 1) * v[m - 1]) % mod;
+        if(sHash < 0)sHash = (sHash + mod) % mod;
+        sHash = (sHash * P + (s[i] - 'a' + 1)) % mod;
+        if(sHash < 0)sHash = (sHash + mod) % mod;
+        if (sHash == tHash && s.substr(i - m + 1, m) == t) return i - m + 1;
+    }
+
+    return -1;
     }
 };
